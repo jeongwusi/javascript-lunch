4c0b8ea664a529e3c1473a672048fb703834a97e
jeongwusi <jeongwusi@gmail.com>
2023-03-13 07:33:49 +0900

    배포

--- src/index.js
+++
@@ -1,5 +0,0 @@
-import App from './app';
-import './components';
-import './style/index.css';
-
-const app = new App();
--- src/utils/localStorage.ts
+++
@@ -1,9 +0,0 @@
-import Restaurant from '../domain/Restaurant';
-
-export const saveRestaurants = (restaurants: Restaurant[]) => {
-  localStorage.setItem('restaurants', JSON.stringify(restaurants));
-};
-
-export const getRestaurants = () => {
-  return JSON.parse(localStorage.getItem('restaurants') || '[]');
-};
